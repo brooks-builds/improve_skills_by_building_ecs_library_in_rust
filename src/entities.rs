c373ee898d691cc9419dcf2c447c96dc9d3ea4f0
Brooks Patton <brooks@brookzerker.com>
2021-08-03 07:42:34 -0600

    adding the bitmask when creating entities

--- src/entities.rs
+++ src/entities.rs
@@ -16,6 +16,7 @@ use crate::custom_errors::CustomErrors;
 pub struct Entities {
     components: HashMap<TypeId, Vec<Option<Rc<RefCell<dyn Any>>>>>,
     bit_masks: HashMap<TypeId, u32>,
+    map: Vec<u32>,
 }
 
 impl Entities {
@@ -30,16 +31,23 @@ impl Entities {
         self.components
             .iter_mut()
             .for_each(|(_key, components)| components.push(None));
+
+        self.map.push(0);
+
         self
     }
 
     pub fn with_component(&mut self, data: impl Any) -> Result<&mut Self> {
         let type_id = data.type_id();
+        let map_index = self.map.len() - 1;
         if let Some(components) = self.components.get_mut(&type_id) {
             let last_component = components
                 .last_mut()
                 .ok_or(CustomErrors::CreateComponentNeverCalled)?;
             *last_component = Some(Rc::new(RefCell::new(data)));
+
+            let bitmask = self.bit_masks.get(&type_id).unwrap();
+            self.map[map_index] |= *bitmask;
         } else {
             return Err(CustomErrors::ComponentNotRegistered.into());
         }
@@ -103,6 +111,24 @@ mod test {
         let borrowed_health = wrapped_health.borrow();
         let health = borrowed_health.downcast_ref::<Health>().unwrap();
         assert_eq!(health.0, 100);
+        Ok(())
+    }
+
+    #[test]
+    fn map_is_updated_when_creating_entities() -> Result<()> {
+        let mut entities = Entities::default();
+        entities.register_component::<Health>();
+        entities.register_component::<Speed>();
+        entities
+            .create_entity()
+            .with_component(Health(100))?
+            .with_component(Speed(15))?;
+        let entity_map = entities.map[0];
+        assert_eq!(entity_map, 3);
+
+        entities.create_entity().with_component(Speed(15))?;
+        let entity_map = entities.map[1];
+        assert_eq!(entity_map, 2);
         Ok(())
     }
 
